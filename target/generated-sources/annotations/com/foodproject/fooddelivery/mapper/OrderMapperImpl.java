fd0c709b184efee2aafabd7b01ba3d72a6a2711c
Nguyen Viet Quyen <jrnguyen14@gmail.com>
2024-11-16 09:55:16 +0700

    update code

--- target/generated-sources/annotations/com/foodproject/fooddelivery/mapper/OrderMapperImpl.java
+++ target/generated-sources/annotations/com/foodproject/fooddelivery/mapper/OrderMapperImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-11-15T09:05:26+0700",
+    date = "2024-11-16T09:51:52+0700",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.1 (Oracle Corporation)"
 )
 @Component
